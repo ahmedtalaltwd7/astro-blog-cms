7f37931ffc4f5e33e45e54474e56fe655fc7082d
Ahmed Talal <76573910+ahmedtalaltwd7@users.noreply.github.com>
2026-05-02 22:22:29 +0300

    Fix Vercel deployment setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
-import node from "@astrojs/node";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), preact()],
   output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: vercel(),
   markdown: {
     shikiConfig: {
       theme: "github-dark",
--- dist/server/pages/_image.astro.mjs
+++
@@ -1,2 +0,0 @@
-export { a as page } from '../chunks/node_Do8hChjf.mjs';
-export { renderers } from '../renderers.mjs';
